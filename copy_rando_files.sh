a7a4cb61199f0efe91d0955ae34a9e1bc371d69e
ardikas <ardika6879@yahoo.com>
2019-04-23 00:42:35 +0000

    script challenge, 2/3, not quiet done with the copy script

---
+++ copy_rando_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Scripting challenge (please commit to git)
+#
+# Copy 1-10 of these newly created files but with different names
+# - The copied files will have the same md5sums as the original files they were copied from
+# Write a script that looks over this directory and finds the duplicate files
+# - Hint: this should work on any directory or files with different filenames
+
+
+#  shuf -n$((1 + RANDOM % 10)) -e File_* | xargs cp -vt ./file_$RANDOM.txt
+
+#files=(./file_*)
+#n=${#files[@]}
+#file_to_copy="${files[1 + RANDOM % 10]}"
+#cp $file_to_copy ./File_copy_$RANDOM.txt
+
+find . -type f -name "file_*" -print0 | xargs -0 shuf -e -n8 -z | xargs -0 cp -vt ./file_copy_$RANDOM.txt
+
+
+
