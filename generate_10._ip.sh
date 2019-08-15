4bac0c962857ca7eb0b2016c0c3f640feaa7395c
ardikas <ardika6879@yahoo.com>
2019-08-15 04:50:53 +0000

    finish finish

--- generate_10._ip.sh
+++ generate_10._ip.sh
@@ -9,24 +9,28 @@
 #seq 1 23 | sed 's/^/10.0.0./'
  #for i in $(seq 1 23); do echo "10.0.0.$i"; done
 
+# First octets
 network_prefix="10."
 
+#second, third, and fourth octets; set to 0
 octet2=0
 octet3=0
 octet4=0
 
-while [[ "$octet2" -le 5 ]]; do
+
+while [[ "$octet2" -le 5 ]]; do # stopped at 5 so it will not print all
 	
 	while [[ "$octet3" -le 255 ]]; do	
 		
 		while [[ "$octet4" -le 255 ]]; do 
 			
+			# %s = append each field. \n = new line;
 			printf "%s%s%s%s%s%s%s%s%s\n" $network_prefix $octet2 "." $octet3 "." $octet4 
-			octet4=$(($octet4+1))
+			octet4=$(($octet4+1)) # increment by 1
 		done
 		
-		octet3=$(($octet3+1))
-		octet4=0
+		octet3=$(($octet3+1)) # increment by 1
+		octet4=0 # reset
 	done
 	
 	octet2=$(($octet2+1))
